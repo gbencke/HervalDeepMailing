76d37b603177898365fa66c3fc7826a2b8b028b2
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-03-28 17:03:38 +0000

    Now the script create the virtualenv if necessary, load the virtualenv and its modules, and then start the jupyter lab

--- start_notebooks.sh
+++ start_notebooks.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+#Create a virtualenv if it doesnt exist
+if [ ! -d "./env" ]; then
+        virtualenv -p python3 env
+fi
+
+#Load the virtualenv created
+source env/bin/activate
+
+#Install the required python modules
+pip install -r requirements
+
+#Start the jupyter lab...
 cd notebook
 jupyter lab --ip='*' --port=8080
