129df22165a2e2b2e35165f1ff4898cca845aeb9
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-04-04 12:11:33 +0000

    script to clean the intermediate and output

---
+++ clean_data_folder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm ./data/batch01/intermediate/*
+rm ./data/batch01/output/*
+
+rm ./data/batch02/model/*
+rm ./data/batch02/intermediate/*
+
+rm ./data/batch03/intermediate/*
+rm ./data/batch03/model/*
+
+rm ./data/output/*
+
