d137e18a88423d80965f4c5f14545786fcc95a51
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-04-04 20:07:54 +0000

    Script para carregar o vim com os paths corretos

---
+++ start_vim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:$PWD/output.source
+export PYTHONPATH=$PYTHONPATH:$PWD/env/lib/python3.5
+export PYTHONPATH=$PYTHONPATH:$PWD/env/lib/python3.5/site-packages
+cd output.source
+vim
+
+
