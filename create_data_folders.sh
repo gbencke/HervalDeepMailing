7e6c9f5b69cf433a4ee07e41023b42acfac68e58
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-04-07 19:19:08 +0000

    Created folders for data2

---
+++ create_data_folders.sh
@@ -0,0 +1,22 @@
+#1/bin/bash
+
+mkdir -p ./data2/batch01
+mkdir -p ./data2/batch01/inputs
+mkdir -p ./data2/batch01/intermediate
+mkdir -p ./data2/batch01/output
+
+mkdir -p ./data2/batch02
+mkdir -p ./data2/batch02/inputs
+mkdir -p ./data2/batch02/intermediate
+mkdir -p ./data2/batch02/output
+
+
+mkdir -p ./data2/batch03
+mkdir -p ./data2/batch03/inputs
+mkdir -p ./data2/batch03/intermediate
+mkdir -p ./data2/batch03/output
+
+mkdir -p ./data2/output
+
+mkdir -p ./logs2
+
